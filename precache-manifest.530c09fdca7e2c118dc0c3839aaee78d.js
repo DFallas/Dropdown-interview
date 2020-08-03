42a76ddaae201325128cdaf014e595033fd9c330
David F <david.fallas@edmodo.com>
2020-08-03 12:57:50 -0600

    Updates

---
+++ precache-manifest.530c09fdca7e2c118dc0c3839aaee78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fa0e1349719d29fffd556b2c8cfc44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "369f5d488f69ec939279",
+    "url": "/static/css/main.9a5001c7.chunk.css"
+  },
+  {
+    "revision": "06a9e6bff2db52efd481",
+    "url": "/static/js/2.692f8c3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.692f8c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369f5d488f69ec939279",
+    "url": "/static/js/main.4aaa7a82.chunk.js"
+  },
+  {
+    "revision": "4306ff603ecedbd7ef56",
+    "url": "/static/js/runtime-main.7e3609b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
