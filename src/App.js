269601155a22de9ec3b2addaae4bb19584964e5f
David F <david.fallas@edmodo.com>
2022-04-23 15:58:51 -0600

    state for each dd, ui  polishes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Arrow from './components/Arrow';
 import './App.css';
 
 function App() {
-  let [currentOption, setCurrentOption] = useState('');
+  let [currentOption1, setCurrentOption1] = useState('');
+  let [currentOption2, setCurrentOption2] = useState('');
+  let [currentOption3, setCurrentOption3] = useState('');
 
   let items = [
     {
@@ -26,29 +28,45 @@ function App() {
     },
   ];
 
-  const dropdownChange = (item) => {
-    setCurrentOption(item.name);
+  const dropdownChange = (id, item) => {
+
+    switch (id) {
+      case '1':
+        setCurrentOption1(item)
+        break;
+      case '2':
+        setCurrentOption2(item)
+        break;
+      case '3':
+        setCurrentOption3(item)
+        break;
+    
+      default:
+        break;
+    }
+
   }
 
-  let dropdownText = (!currentOption || currentOption === 'none') ? 'Dropdown': items.filter((item)=>currentOption===item.name)[0].text;
+  let dropdownText1 = (!currentOption1 || currentOption1 === 'none') ? 'Dropdown': items.filter((item)=>currentOption1.name===item.name)[0].text;
+  let dropdownText2 = (!currentOption2 || currentOption3 === 'none') ? 'Dropdown': items.filter((item)=>currentOption2.name===item.name)[0].text;
+  let dropdownText3 = (!currentOption3 || currentOption3 === 'none') ? 'Dropdown': items.filter((item)=>currentOption3.name===item.name)[0].text;
   return (
     <div className="App">
       <header className="App-header">
         <p className='signature'>
-          David Fallas's LogN interview
+        General Base DropDown Component
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
       </header>
 
       <div className='column'>
-        <h3>General Base DropDown Component</h3>
+      <h3>Default Version</h3>
         <Dropdown className=''>
-            <span className='elipsis'>{dropdownText}</span>
+            <span className='elipsis'>{dropdownText1}</span>
             <DropDownOptionList>
             {
               items.map((item, index)=>{
                 return (
-                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange(item)}>
+                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange('1',item)}>
                     <span className='elipsis'>{item.text}</span>
                   </DropDownItem>
         
@@ -64,13 +82,13 @@ function App() {
         <div className ='row'> 
           <p>Testing layout</p>
           <Dropdown className='pixel-perfect'>
-            <span className='elipsis'>{dropdownText}</span>
+            <span className='elipsis'>{dropdownText2}</span>
             <Arrow className='dropdown' position={'down'} color={'white'}/>
             <DropDownOptionList>
             {
               items.map((item, index)=>{
                 return (
-                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange(item)}>
+                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange('2',item)}>
                     <span className='elipsis'>{item.text}</span>
                   </DropDownItem>
         
@@ -83,13 +101,13 @@ function App() {
         <div className ='row'> 
           <p>With Animation</p>
           <Dropdown className='pixel-perfect' animation={true}>
-            <span className='elipsis'>{dropdownText}</span>
+            <span className='elipsis'>{dropdownText3}</span>
             <Arrow className='dropdown' position={'down'} color={'white'}/>
             <DropDownOptionList>
             {
               items.map((item, index)=>{
                 return (
-                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange(item)}>
+                  <DropDownItem key={`${item.name}-${index}-${Date.now()}`} onClick={()=>dropdownChange('3',item)}>
                     <span className='elipsis'>{item.text}</span>
                   </DropDownItem>
         
